e697923c27d0e346e04e1a41a253e1cdd57203c0
dbinheaven <36320167+dbinheaven@users.noreply.github.com>
2021-12-12 19:45:02 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -4,7 +4,7 @@ aria2c --enable-rpc --check-certificate=false \
    --bt-stop-timeout=0 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=0 --bt-tracker="[$tracker_list]"\
    --max-overall-upload-limit=1K --max-concurrent-downloads=2 --continue=true \
-   --peer-id-prefix=-qB4380- --user-agent=Chrome/95.0.4638.54 --peer-agent=qBittorrent/4.3.8 \
+   --peer-id-prefix=-qB4380- --user-agent=Transmission/2.77 --peer-agent=qBittorrent/4.3.8 \
    --disk-cache=32M --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 \
    --max-tries=20 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true \
    --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc
